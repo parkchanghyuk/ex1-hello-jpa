9375da63f605e6133c1ab257f08dc9b3b40a48d0
changhyuk <changchang743@gmail.com>
2023-01-03 23:58:36 +0900

    study: JPA 초기 세팅

--- src/main/java/JpaMain.java
+++
@@ -1,8 +0,0 @@
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
-public class JpaMain {
-	public static void main(String[] args) {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-	}
-}
